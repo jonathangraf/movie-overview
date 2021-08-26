0f27d9e8fc06d829320b080f04330a8ae931b8a8
Jonathan Graf <jonathangraf57@gmail.com>
2021-08-26 17:07:02 +0200

    reconfidure project

--- src/config/aliases.js
+++
@@ -1,9 +0,0 @@
-module.exports = (prefix = '.src/') => ({
-	'@atoms': `${prefix}/components/atoms`,
-	'@components': `${prefix}/components`,
-	'@hooks': `${prefix}/hooks`,
-	'@molecules': `${prefix}/components/molecules`,
-	'@organisms': `${prefix}/components/organisms`,
-	'@templates': `${prefix}/components/templates`,
-	'@types': `${prefix}/types`,
-});
